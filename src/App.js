7101f3ff1b668c6f001f9485532cef3144326f74
eunchan <pud1251@gmail.com>
2022-04-05 10:14:49 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  Navbar,
+  Container,
+  Offcanvas,
+  Nav,
+  Form,
+  Button,
+  NavDropdown,
+  FormControl,
+} from "react-bootstrap";
+import productData from "./data.js";
+import { DetailPageItem } from "./detailPageItems.js";
+import { Link, Route, Switch } from "react-router-dom";
+
+function App() {
+  let [product, setProduct] = useState(productData);
+
+  return (
+    <div className="App">
+      <NewNavbar />
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron />
+          <ShopItemList product={product} />
+        </Route>
+        <Route path="/detail/:id">
+          <DetailPageItem product={product} />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function NewNavbar() {
+  return (
+    <Navbar bg="white" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#">Shoe Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link as={Link} className="link" to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} className="link" to="detail">
+                Detail
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">premium shoe market!</h1>
+      <p className="lead">All the shoes you want are on our page</p>
+      <hr className="my-4" />
+      <p>
+        Reasonable price <br />
+        The highest quality
+      </p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Buy now
+        </a>
+      </p>
+    </div>
+  );
+}
+
+function ShopItemList(props) {
+  return (
+    <div className="container">
+      <div className="row shopItemList">
+        {props.product.map((e, i) => {
+          return <ShopItems product={e} index={i} key={e.id} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function ShopItems(props) {
+  return (
+    <div className="col-md-4 shopItem">
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`}
+      />
+      <h5>{props.product.title}</h5>
+      <p>{props.product.content}</p>
+      <p>{props.product.price}</p>
+    </div>
+  );
+}
+
+// function DetailPage(props) {
+//   {
+//     return props.product.map((e, i) => {
+//       return <DetailPageItems index={i} product={e} />;
+//     });
+//   }
+// }
+
+// function DetailPageItems(props) {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-md-6">
+//           <img
+//             src={`https://codingapple1.github.io/shop/shoes${
+//               props.index + 1
+//             }.jpg`}
+//             width="100%"
+//           />
+//         </div>
+//         <div className="col-md-6 mt-4">
+//           <h4 className="pt-5">{props.product.title}</h4>
+//           <p>{props.product.content}</p>
+//           <p>{props.product.price}</p>
+//           <button className="btn btn-danger">주문하기</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ src/detailPageItems.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+export { DetailPageItem };
+
+let 박스 = styled.div`
+  padding-top: 30px;
+`;
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.color};
+`;
+
+function DetailPageItem(props) {
+  let { id } = useParams();
+  let history = useHistory();
+  let newProduct = props.product.find(function (e) {
+    return e.id == id;
+  });
+  let [alert, setAlert] = useState(true);
+  let [inputVal, setInputVal] = useState("");
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+      setAlert(true);
+    };
+  }, alert);
+
+  return (
+    <div className="container">
+      <박스>
+        <제목 color="black">Detail</제목>
+      </박스>
+      {inputVal}
+      <input
+        onChange={(e) => {
+          setInputVal(e.target.value);
+        }}
+      />
+      {alert ? <div className="myAlert">almost out of stock</div> : null}
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={`https://codingapple1.github.io/shop/shoes${
+              newProduct.id + 1
+            }.jpg`}
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{newProduct.title}</h4>
+          <p>{newProduct.content}</p>
+          <p>{newProduct.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
